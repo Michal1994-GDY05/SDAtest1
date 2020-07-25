44f7d5c5d1678f490b1c354740b00910647e8e96
Matylda <matylda.badowska@gmail.com>
2020-07-25 10:00:32 +0200

    lastNameMB

--- src/main/java/Person/Person.java
+++ src/main/java/Person/Person.java
@@ -2,9 +2,12 @@ package Person;
 
 public class Person {
     String name;
+    String lastName;
 
-    public Person(String name) {
+
+    public Person(String name, String lastName) {
         this.name = name;
+        this.lastName = lastName;
     }
 
     public String getName() {
@@ -13,5 +16,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
