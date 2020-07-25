98772d15b535c1b72be5567d4fe35601b02dd0ee
zawadaaleksandra93 <zawadaaleksandra93@users.noreply.github.com>
2020-07-25 09:56:48 +0200

    zawadaAleksandra_add_age

--- src/main/java/Person/Person.java
+++ src/main/java/Person/Person.java
@@ -2,6 +2,24 @@ package Person;
 
 public class Person {
     String name;
+    int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public Person(String name) {
         this.name = name;
